cf40463c4188d9a6de6a97e7eca208b6a94359f5
Paul Baird <Paul19988@users.noreply.github.com>
2022-07-07 01:26:41 +0100

    Add relocation for Lettuce

--- slimeworldmanager-plugin/build.gradle.kts
+++ slimeworldmanager-plugin/build.gradle.kts
@@ -31,6 +31,7 @@ tasks {
         relocate("com.flowpowered.nbt", "com.grinderwolf.swm.internal.nbt")
         relocate("com.zaxxer.hikari", "com.grinderwolf.swm.internal.hikari")
         relocate("com.mongodb", "com.grinderwolf.swm.internal.mongodb")
+        relocate("io.lettuce", "com.grinderwolf.swm.internal.lettuce")
         relocate("org.bson", "com.grinderwolf.swm.internal.bson")
     }
 
