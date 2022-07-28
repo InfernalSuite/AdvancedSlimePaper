381f8399a08e5fc6cdc9415a435d3220d1186809
Philip Kelley <philip@tubbo.net>
2022-07-28 20:41:29 +0100

    Update plugin to include 1.19.1 NMS Module

--- slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/SWMPlugin.java
+++ slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/SWMPlugin.java
@@ -14,6 +14,7 @@ import com.grinderwolf.swm.nms.v1171.v1171SlimeNMS;
 import com.grinderwolf.swm.nms.v1181.v1181SlimeNMS;
 import com.grinderwolf.swm.nms.v1182.v1182SlimeNMS;
 import com.grinderwolf.swm.nms.v119.v119SlimeNMS;
+import com.grinderwolf.swm.nms.v1191.v1191SlimeNMS;
 import com.grinderwolf.swm.nms.world.SlimeLoadedWorld;
 import com.grinderwolf.swm.plugin.commands.CommandManager;
 import com.grinderwolf.swm.plugin.config.ConfigManager;
@@ -186,6 +187,7 @@ public class SWMPlugin extends JavaPlugin implements SlimePlugin, Listener {
             case 2865 -> new v1181SlimeNMS(isPaperMC);
             case 2975 -> new v1182SlimeNMS(isPaperMC);
             case 3105 -> new v119SlimeNMS(isPaperMC);
+            case 3117 -> new v1191SlimeNMS(isPaperMC);
             default -> throw new InvalidVersionException("" + dataVersion);
         };
     }
