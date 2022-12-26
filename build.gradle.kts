cce4215df8f1cd74016b41211a344a63662f7689
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2022-12-26 11:30:41 -0500

    Initial Built Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,68 @@
 plugins {
-    `java-library`
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("io.papermc.paperweight.patcher") version "1.4.0"
     id("org.kordamp.gradle.profiles") version "0.47.0"
 }
 
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-        mavenCentral()
-    }
-    dependencies {
-        classpath("io.freefair.gradle:lombok-plugin:6.3.0")
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
 
 allprojects {
-
-    group = "com.grinderwolf"
-    version = "2.10.0-SNAPSHOT"
-
     apply(plugin = "java")
-    apply(plugin = "io.freefair.lombok")
-    apply(plugin = "org.kordamp.gradle.profiles")
-
-    repositories {
-        mavenLocal()
-        mavenCentral()
-
-        maven("https://repo.papermc.io/repository/maven-public/")
-        maven("https://repo.codemc.io/repository/nms/")
-        maven("https://repo.rapture.pw/repository/maven-releases/")
-        maven("https://repo.glaremasters.me/repository/concuncan/")
-    }
-
-    tasks.withType<JavaCompile> {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
-
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-        (options as StandardJavadocDocletOptions).tags("apiNote:a:API Note:", "implSec:a:Implementation Requirements:", "implNote:a:Implementation Note:")
-    }
-
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+}
+
+paperweight {
+    serverProject.set(project(":slimeworldmanager-server"))
+
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-api"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-server"))
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,11 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
-
 }
 
-
 rootProject.name = "slimeworldmanager"
-include(":slimeworldmanager-api")
-include(":slimeworldmanager-classmodifier")
-include(":slimeworldmanager-classmodifierapi")
-include(":slimeworldmanager-plugin")
-include(":slimeworldmanager-importer")
-include(":slimeworldmanager-nms-common")
-include(":slimeworldmanager-nms-v118-2")
-include("slimeworldmanager-nms-v119")
-include("slimeworldmanager-nms-v119-1")
-include("slimeworldmanager-nms-v119-2")
+
+include("api")
+include("slimeworldmanager-api", "slimeworldmanager-server")
