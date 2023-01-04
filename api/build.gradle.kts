8151d8414b40ce875208384a8b1f8d69b323e6b2
b0ykoe <b0ykoe@ymail.com>
2023-01-04 20:47:42 +0100

    feat: added javadocs to repo

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,6 +11,11 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 profiles {
     profile("publish") {
         activation {
