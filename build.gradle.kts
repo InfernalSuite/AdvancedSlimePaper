bfe262f26de424c01f2417fec6346094b94bee10
Paul <Paul19988@users.noreply.github.com>
2025-03-09 18:37:43 +0000

    hard fork improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,67 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     java
     `maven-publish`
     id("com.gradleup.shadow") version "8.3.5" apply false
-    id("io.papermc.paperweight.patcher") version "1.7.7"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.14"
     id("org.kordamp.gradle.profiles") version "0.54.0"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
+paperweight {
+    upstreams.paper {
+        ref = providers.gradleProperty("paperCommit")
+
+        patchFile {
+            path = "paper-server/build.gradle.kts"
+            outputFile = file("slimeworldmanager-server/build.gradle.kts")
+            patchFile = file("slimeworldmanager-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "paper-api/build.gradle.kts"
+            outputFile = file("slimeworldmanager-api/build.gradle.kts")
+            patchFile = file("slimeworldmanager-api/build.gradle.kts.patch")
+        }
+        patchDir("paperApi") {
+            upstreamPath = "paper-api"
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("slimeworldmanager-api/paper-patches")
+            outputDir = file("paper-api")
+        }
+    }
+}
+
+//paperweight {
+//    serverProject.set(project(":slimeworldmanager-server"))
+//
+//    remapRepo.set(paperMavenPublicUrl)
+//    decompileRepo.set(paperMavenPublicUrl)
+//
+//    usePaperUpstream(providers.gradleProperty("paperRef")) {
+//        withPaperPatcher {
+//            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+//            apiOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-api"))
+//
+//            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+//            serverOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-server"))
+//
+//            patchTasks {
+//                register("generatedApi") {
+//                    isBareDirectory.set(true)
+//                    upstreamDirPath.set("paper-api-generator/generated")
+//                    patchDir.set(layout.projectDirectory.dir("patches/generatedApi"))
+//                    outputDir.set(layout.projectDirectory.dir("paper-api-generator/generated"))
+//                }
+//            }
+//        }
+//    }
+//}
+
 repositories {
     mavenCentral()
-    maven(paperMavenPublicUrl) {
-        content { onlyForConfigurations(configurations.paperclip.name) }
-    }
 }
 
 allprojects {
@@ -38,16 +87,11 @@ allprojects {
     }
 }
 
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
-    decompiler("org.vineflower:vineflower:1.10.1")
-    paperclip("io.papermc:paperclip:3.0.3")
-}
-
 subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(21)
+        options.isFork = true
     }
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
@@ -55,35 +99,21 @@ subprojects {
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
+        }
+    }
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
 
     repositories {
         mavenCentral()
         maven(paperMavenPublicUrl)
-    }
-}
-
-paperweight {
-    serverProject.set(project(":slimeworldmanager-server"))
-
-    remapRepo.set(paperMavenPublicUrl)
-    decompileRepo.set(paperMavenPublicUrl)
-
-    usePaperUpstream(providers.gradleProperty("paperRef")) {
-        withPaperPatcher {
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-api"))
-
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("slimeworldmanager-server"))
-
-            patchTasks {
-                register("generatedApi") {
-                    isBareDirectory.set(true)
-                    upstreamDirPath.set("paper-api-generator/generated")
-                    patchDir.set(layout.projectDirectory.dir("patches/generatedApi"))
-                    outputDir.set(layout.projectDirectory.dir("paper-api-generator/generated"))
-                }
-            }
-        }
+        maven("https://jitpack.io")
     }
 }
