983129954c511db0d619a9fa7088e9a92ca6ced7
David <davidliebtkekse@gmail.com>
2025-07-21 12:12:59 +0200

    feat: use consistent uuids for slime worlds

--- aspaper-server/src/main/java/com/infernalsuite/asp/level/SlimeBootstrap.java
+++ aspaper-server/src/main/java/com/infernalsuite/asp/level/SlimeBootstrap.java
@@ -2,7 +2,15 @@ package com.infernalsuite.asp.level;
 
 import com.infernalsuite.asp.api.world.SlimeWorld;
 
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
 public record SlimeBootstrap(
         SlimeWorld initial
 ) {
+
+    public UUID generateId() {
+        return UUID.nameUUIDFromBytes(("SlimeWorld" + initial.getName()).getBytes(StandardCharsets.UTF_8));
+    }
+
 }
