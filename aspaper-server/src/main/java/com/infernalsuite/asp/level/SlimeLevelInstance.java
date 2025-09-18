150b62c0bdacdb085464907039eeee2f7778badf
David <davidliebtkekse@gmail.com>
2025-09-18 20:09:08 +0200

    fix: set spawn point methods

--- aspaper-server/src/main/java/com/infernalsuite/asp/level/SlimeLevelInstance.java
+++ aspaper-server/src/main/java/com/infernalsuite/asp/level/SlimeLevelInstance.java
@@ -18,7 +18,9 @@ import com.infernalsuite.asp.api.world.properties.SlimeProperties;
 import com.infernalsuite.asp.api.world.properties.SlimePropertyMap;
 import net.kyori.adventure.nbt.BinaryTag;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.kyori.adventure.util.TriState;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.GlobalPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
@@ -26,6 +28,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.ProgressListener;
 import net.minecraft.util.datafix.DataFixers;
 import net.minecraft.world.Difficulty;
@@ -35,6 +38,7 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.chunk.storage.RegionStorageInfo;
 import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.PrimaryLevelData;
 import net.minecraft.world.level.validation.DirectoryValidator;
@@ -55,6 +59,7 @@ import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.logging.Level;
 
 public class SlimeLevelInstance extends ServerLevel {
@@ -95,11 +100,21 @@ public class SlimeLevelInstance extends ServerLevel {
         SlimePropertyMap propertyMap = slimeBootstrap.initial().getPropertyMap();
 
         this.serverLevelData.setDifficulty(Difficulty.valueOf(propertyMap.getValue(SlimeProperties.DIFFICULTY).toUpperCase()));
-//        this.serverLevelData.setSpawn(new BlockPos(
-//                        propertyMap.getValue(SlimeProperties.SPAWN_X),
-//                        propertyMap.getValue(SlimeProperties.SPAWN_Y),
-//                        propertyMap.getValue(SlimeProperties.SPAWN_Z)),
-//                propertyMap.getValue(SlimeProperties.SPAWN_YAW)); //TODO:
+        serverLevelData.setSpawn(
+                new LevelData.RespawnData(
+                        GlobalPos.of(
+                                ResourceKey.create(Registries.DIMENSION, this.dimension().location()),
+                                new BlockPos(
+                                        propertyMap.getValue(SlimeProperties.SPAWN_X),
+                                        propertyMap.getValue(SlimeProperties.SPAWN_Y),
+                                        propertyMap.getValue(SlimeProperties.SPAWN_Z)
+                                )
+                        ),
+                        Mth.wrapDegrees(propertyMap.getValue(SlimeProperties.SPAWN_YAW)),
+                        Mth.wrapDegrees(0F)
+                )
+        );
+
         super.chunkSource.setSpawnSettings(propertyMap.getValue(SlimeProperties.ALLOW_MONSTERS), propertyMap.getValue(SlimeProperties.ALLOW_ANIMALS));
 
         ConcurrentMap<String, BinaryTag> extraData = this.slimeInstance.getExtraData();
@@ -108,7 +123,7 @@ public class SlimeLevelInstance extends ServerLevel {
             getWorld().readBukkitValues(Converter.convertTag(extraData.get("BukkitValues")));
         }
 
-//        this.pvpMode = propertyMap.getValue(SlimeProperties.PVP); //TODO:
+        this.pvpMode = propertyMap.getOptionalValue(SlimeProperties.PVP).map(TriState::byBoolean).orElse(TriState.NOT_SET);
 
         this.entityDataController = new SlimeEntityDataLoader(
                 new ca.spottedleaf.moonrise.patches.chunk_system.io.datacontroller.EntityDataController.EntityRegionFileStorage(
@@ -197,18 +212,6 @@ public class SlimeLevelInstance extends ServerLevel {
     public ChunkDataLoadTask getLoadTask(ChunkLoadTask task, ChunkTaskScheduler scheduler, ServerLevel world, int chunkX, int chunkZ, Priority priority, Consumer<GenericDataLoadTask.TaskResult<ChunkAccess, Throwable>> onRun) {
         return new ChunkDataLoadTask(task, scheduler, world, chunkX, chunkZ, priority, onRun);
     }
-
-    //TODO:
-//    @Override
-//    public void setDefaultSpawnPos(BlockPos pos, float angle) {
-//        super.setDefaultSpawnPos(pos, angle);
-//
-//        SlimePropertyMap propertyMap = this.slimeInstance.getPropertyMap();
-//        propertyMap.setValue(SlimeProperties.SPAWN_X, pos.getX());
-//        propertyMap.setValue(SlimeProperties.SPAWN_Y, pos.getY());
-//        propertyMap.setValue(SlimeProperties.SPAWN_Z, pos.getZ());
-//        propertyMap.setValue(SlimeProperties.SPAWN_YAW, angle);
-//    }
 
     public void deleteTempFiles() {
         WORLD_SAVER_SERVICE.execute(() -> {
