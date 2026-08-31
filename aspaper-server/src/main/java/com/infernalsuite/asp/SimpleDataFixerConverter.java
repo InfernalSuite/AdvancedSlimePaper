1d40a867b110bbff4361db7e598b7028710a4547
David <davidliebtkekse@gmail.com>
2026-08-31 02:26:58 +0200

    fix: compile issues with the data converter thingy

--- aspaper-server/src/main/java/com/infernalsuite/asp/SimpleDataFixerConverter.java
+++ aspaper-server/src/main/java/com/infernalsuite/asp/SimpleDataFixerConverter.java
@@ -1,9 +1,9 @@
 package com.infernalsuite.asp;
 
-import ca.spottedleaf.dataconverter.converters.DataConverter;
+import ca.spottedleaf.converter.DataConverter;
+import ca.spottedleaf.converter.types.MapType;
 import ca.spottedleaf.dataconverter.minecraft.datatypes.MCTypeRegistry;
 import ca.spottedleaf.dataconverter.minecraft.walkers.generic.WalkerUtils;
-import ca.spottedleaf.dataconverter.types.MapType;
 import ca.spottedleaf.dataconverter.types.nbt.NBTListType;
 import ca.spottedleaf.dataconverter.types.nbt.NBTMapType;
 import com.infernalsuite.asp.api.SlimeDataConverter;
